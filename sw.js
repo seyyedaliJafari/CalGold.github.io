534127fe9c84175aada45aff23cb35abb117360c
Ali Jafari <alisgm1391@gmail.com>
2025-11-14 13:35:33 +0330

    Update sw.js
    Signed-off-by: Ali Jafari <alisgm1391@gmail.com>

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
-self.addEventListener("install", (e) => {
-    self.skipWaiting();
+const cacheName = 'gold-calculator-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  'https://fs.noorgram.ir/xen/2021/05/1937_a04f325674750b15e1a00d49222b587f.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-    return clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
